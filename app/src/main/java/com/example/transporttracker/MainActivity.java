ea8dac74401c512833cbba0ddb2e3780bc31ba04
Sergey Ushakov <sergey.ushakov94@gmail.com>
2020-02-05 13:10:36 +0200

    Creating markers on the map everytime map is moved

--- app/src/main/java/com/example/transporttracker/MainActivity.java
+++ app/src/main/java/com/example/transporttracker/MainActivity.java
@@ -8,6 +8,13 @@ import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.hsl.TestQuery;
 
 import org.jetbrains.annotations.NotNull;
@@ -15,30 +22,85 @@ import org.jetbrains.annotations.NotNull;
 import okhttp3.OkHttpClient;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String BASE_URL = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
+
+    ApolloClient apolloClient;
+    GoogleMap googleMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initApolloClient();
+        initMap();
+
+    }
+
+    void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    void initApolloClient() {
         OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
-        ApolloClient apolloClient = ApolloClient.builder()
+
+        apolloClient = ApolloClient.builder()
                 .serverUrl(BASE_URL)
                 .okHttpClient(okHttpClient)
                 .build();
+    }
+
+    public void onMapReady(final GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng home = new LatLng(60.206723, 24.667192);
 
 
-        TestQuery testQuery = TestQuery.builder().build();
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(14));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+
+        googleMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                doQuery(googleMap.getCameraPosition().target);
+            }
+        });
+
+        this.doQuery(new LatLng(60.206723, 24.667192));
+    }
+
+    void addStopMarker(final TestQuery.Stop stop) {
+        final LatLng stopLocation = new LatLng(
+                stop.lat(),
+                stop.lon()
+        );
+
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                googleMap.addMarker(
+                    new MarkerOptions().position(stopLocation).title(stop.name())
+                );
+            }
+        });
+    }
+
+    void doQuery(LatLng latLng) {
+        TestQuery testQuery = TestQuery.builder().lat(latLng.latitude).lon(latLng.longitude).build();
 
         apolloClient.query(testQuery).enqueue(new ApolloCall.Callback<TestQuery.Data>() {
 
+
             @Override
             public void onResponse(@NotNull Response<TestQuery.Data> response) {
-                final StringBuffer buffer = new StringBuffer();
                 for (TestQuery.Edge edge : response.data().stopsByRadius().edges()) {
-                    System.out.println(edge.node().stop().name());
+                    addStopMarker(edge.node().stop());
                 }
+
             }
 
             @Override
@@ -46,6 +108,5 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(e);
             }
         });
-
     }
 }
