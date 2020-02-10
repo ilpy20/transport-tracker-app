78ab88e3d39dc96030e8f8f2405cdcfd40ea4289
Ilya Pyshkin <19843442+ilpy20@users.noreply.github.com>
2020-02-10 20:16:49 +0200

    delete from action bar and make status bar transparent with dark text

--- app/src/main/java/com/example/transporttracker/MainActivity.java
+++ app/src/main/java/com/example/transporttracker/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.transporttracker;
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Response;
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     initMap();
