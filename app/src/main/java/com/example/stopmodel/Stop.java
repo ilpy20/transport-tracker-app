ad6b82fbc9894f0e8bb7dcef8ba7057c1e3e5ca3
Ilya Pyshkin <19843442+ilpy20@users.noreply.github.com>
2020-02-20 19:11:03 +0200

    creating stop and transport classes

--- app/src/main/java/com/example/stopmodel/Stop.java
+++ app/src/main/java/com/example/stopmodel/Stop.java
@@ -1,4 +1,59 @@
 package com.example.stopmodel;
 
 public class Stop {
+  private String stopId;
+  private String stopName;
+  private String stopCode;
+  private String zoneId;
+  private String vehicleMode;
+  private int platformCode;
+
+  public void setStopId(String stopId) {
+    this.stopId = stopId;
+  }
+
+  public String getStopId() {
+    return stopId;
+  }
+
+  public void setStopName(String stopName) {
+    this.stopName = stopName;
+
+  }
+
+  public String getStopName() {
+    return stopName;
+  }
+
+  public void setStopCode(String stopCode) {
+    this.stopCode = stopCode;
+  }
+
+  public String getStopCode() {
+    return stopCode;
+  }
+
+  public void setZoneId(String zoneId) {
+    this.zoneId = zoneId;
+  }
+
+  public String getZoneId() {
+    return zoneId;
+  }
+
+  public void setVehicleMode(String vehicleMode) {
+    this.vehicleMode = vehicleMode;
+  }
+
+  public String getVehicleMode() {
+    return vehicleMode;
+  }
+
+  public void setPlatformCode(int platformCode) {
+    this.platformCode = platformCode;
+  }
+
+  public int getPlatformCode() {
+    return platformCode;
+  }
 }
