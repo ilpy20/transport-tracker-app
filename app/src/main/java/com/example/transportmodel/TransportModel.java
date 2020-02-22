09e9d776c61373005bd35aa611eac7d60c65df39
Sergey Ushakov <sergey.ushakov94@gmail.com>
2020-02-22 22:36:52 +0200

    changed TransportModel to have only one active subscription

--- app/src/main/java/com/example/transportmodel/TransportModel.java
+++ app/src/main/java/com/example/transportmodel/TransportModel.java
@@ -1,6 +1,6 @@
 package com.example.transportmodel;
 
-import android.telecom.Call;
+import android.util.Log;
 
 import com.apollographql.apollo.ApolloSubscriptionCall;
 import com.apollographql.apollo.api.Response;
@@ -12,53 +12,69 @@ import com.hsl.TransportSubscription;
 import org.jetbrains.annotations.NotNull;
 
 public class TransportModel {
-    private TransportSubscription initializeSubscription(LatLng farLeft, LatLng nearRight) {
-        return TransportSubscription.builder()
-                .minLon(farLeft.longitude)
-                .minLat(nearRight.latitude)
-                .maxLat(farLeft.latitude)
-                .maxLon(nearRight.longitude)
-                .build();
+  private static final float COORDINATE_PADDING = 0.01f;
+  private ApolloSubscriptionCall<TransportSubscription.Data> subscriptionInstance = null;
+
+  private TransportSubscription initializeSubscription(LatLng farLeft, LatLng nearRight) {
+    return TransportSubscription.builder()
+      .minLon(farLeft.longitude + COORDINATE_PADDING)
+      .minLat(nearRight.latitude + COORDINATE_PADDING)
+      .maxLat(farLeft.latitude + COORDINATE_PADDING)
+      .maxLon(nearRight.longitude + COORDINATE_PADDING)
+      .build();
+  }
+
+
+  private void subscribeToEvents(TransportSubscription transportSubscription, final Callback callback) {
+    subscriptionInstance = Networking.apollo().subscribe(transportSubscription);
+
+    subscriptionInstance.execute(new ApolloSubscriptionCall.Callback<TransportSubscription.Data>() {
+      @Override
+      public void onResponse(@NotNull Response<TransportSubscription.Data> response) {
+        callback.onEvent(response.data().transportEventsInArea());
+      }
+
+      @Override
+      public void onFailure(@NotNull ApolloException e) {
+        callback.onError(e);
+      }
+
+      @Override
+      public void onCompleted() {
+
+      }
+
+      @Override
+      public void onTerminated() {
+
+      }
+
+      @Override
+      public void onConnected() {
+        System.out.println("Connected to subscription");
+      }
+    });
+  }
+
+  public void subscribeToTransportEvents(LatLng farLeft, LatLng nearRight, Callback callback) {
+    if (subscriptionInstance != null) {
+      terminateSubscription();
     }
+    subscribeToEvents(
+      initializeSubscription(farLeft, nearRight),
+      callback
+    );
+  }
 
-    private void subscribeToEvents(TransportSubscription transportSubscription, final Callback callback) {
-        Networking.apollo().subscribe(transportSubscription).execute(new ApolloSubscriptionCall.Callback<TransportSubscription.Data>() {
-            @Override
-            public void onResponse(@NotNull Response<TransportSubscription.Data> response) {
-                callback.onEvent(response.data().transportEventsInArea());
-            }
-
-            @Override
-            public void onFailure(@NotNull ApolloException e) {
-                callback.onError(e);
-            }
-
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onTerminated() {
-
-            }
-
-            @Override
-            public void onConnected() {
-                System.out.println("Connected to subscription");
-            }
-        });
+  public void terminateSubscription() {
+    if (subscriptionInstance != null) {
+      subscriptionInstance.cancel();
     }
+  }
 
-    public void subscribeToTransportEvents(LatLng farLeft, LatLng nearRight, Callback callback) {
-        subscribeToEvents(
-                initializeSubscription(farLeft, nearRight),
-                callback
-        );
-    }
+  public interface Callback {
+    void onEvent(TransportSubscription.TransportEventsInArea transportEvent);
 
-    public interface Callback {
-        void onEvent(TransportSubscription.TransportEventsInArea transportEvent);
-        void onError(@NotNull ApolloException e);
-    }
+    void onError(@NotNull ApolloException e);
+  }
 }
