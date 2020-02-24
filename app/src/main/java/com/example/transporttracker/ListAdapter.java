4cbc85bbf5b019ddc6720c96a533623847e888f1
Ilya Pyshkin <19843442+ilpy20@users.noreply.github.com>
2020-02-24 13:57:48 +0200

    now bottom sheet shows when he come

--- app/src/main/java/com/example/transporttracker/ListAdapter.java
+++ app/src/main/java/com/example/transporttracker/ListAdapter.java
@@ -20,27 +20,33 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> 
   private ArrayList<String> mNum;
   private ArrayList<String> mName;
   private ArrayList<String> mTime;
+  private ArrayList<String> mDelay;
 
   public static class MyViewHolder extends RecyclerView.ViewHolder {
 
     TextView num;
     TextView name;
+    TextView time;
+    TextView delay;
     ImageView imgMode;
 
     public MyViewHolder(View itemView) {
       super(itemView);
-
+      this.imgMode = (ImageView) itemView.findViewById(R.id.modeRV);
       this.num = (TextView) itemView.findViewById(R.id.numRV);
       this.name = (TextView) itemView.findViewById(R.id.nameRV);
-      this.imgMode = (ImageView) itemView.findViewById(R.id.modeRV);
+      this.time = itemView.findViewById(R.id.timeRV);
+      this.delay = itemView.findViewById(R.id.delay);
     }
   }
 
-  public ListAdapter(Context mContext,ArrayList<String> num,ArrayList<String> name) {
+  public ListAdapter(Context mContext,ArrayList<String> num,ArrayList<String> name,ArrayList<String> time,ArrayList<String> delay) {
     this.mContext = mContext;
     //this.mMode = mode;
     this.mNum = num;
     this.mName = name;
+    this.mTime = time;
+    this.mDelay = delay;
   }
 
   @Override
@@ -55,7 +61,8 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> 
   public void onBindViewHolder(final MyViewHolder holder, final int i) {
     holder.num.setText(mNum.get(i));
     holder.name.setText(mName.get(i));
-
+    holder.time.setText(mTime.get(i));
+    holder.delay.setText(mDelay.get(i));
 
     holder.itemView.setOnClickListener(new View.OnClickListener() {
       @Override
